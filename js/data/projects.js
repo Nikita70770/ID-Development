e614dd2780c57c5f7cabf7d5fd583f2466e44ae0
Nikita <filippovni1999@gmail.com>
2023-04-20 19:51:17 +0300

    feat: Add the projects.js file which contains an array of objects

---
+++ js/data/projects.js
@@ -0,0 +1,56 @@
+let portfolio = [
+    {
+        name: "Project#1",
+        tags: "web-projects",
+        fieldOfActivity: "",
+        imageProject: "img/projects/web-projects/project_web_(1).jpg"
+    },
+    {
+        name: "Project#2",
+        tags: "applications",
+        fieldOfActivity: "",
+        imageProject: "img/projects/applications/project_app_(1).jpg"
+    },
+    {
+        name: "Project#3",
+        tags: "web-projects",
+        fieldOfActivity: "",
+        imageProject: "img/projects/web-projects/project_web_(2).jpg"
+    },
+    {
+        name: "Project#4",
+        tags: "applications",
+        fieldOfActivity: "",
+        imageProject: "img/projects/applications/project_app_(2).jpg"
+    },
+    {
+        name: "Project#5",
+        tags: "web-projects",
+        fieldOfActivity: "",
+        imageProject: "img/projects/web-projects/project_web_(3).jpg"
+    },
+    {
+        name: "Project#6",
+        tags: "applications",
+        fieldOfActivity: "",
+        imageProject: "img/projects/applications/project_app_(3).jpg"
+    },
+    {
+        name: "Project#7",
+        tags: "web-projects",
+        fieldOfActivity: "",
+        imageProject: "img/projects/web-projects/project_web_(4).jpg"
+    },
+    {
+        name: "Project#8",
+        tags: "applications",
+        fieldOfActivity: "",
+        imageProject: "img/projects/applications/project_app_(4).jpg"
+    },
+    {
+        name: "Project#9",
+        tags: "applications",
+        fieldOfActivity: "",
+        imageProject: "img/projects/applications/project_app_(5).jpg"
+    }
+]
