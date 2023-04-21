0210d1e346eee26a21fafbde336f8dbc668c8309
Nikita <filippovni1999@gmail.com>
2023-04-21 10:09:10 +0300

    feat: Make small changes to files index.html , style.css and project.js

--- js/projects.js
+++ js/projects.js
@@ -1,56 +1,70 @@
 let portfolio = [
     {
         name: "Project#1",
-        tags: "web-projects",
+        type: "web-projects",
         fieldOfActivity: "",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
         imageProject: "img/projects/web-projects/project_web_(1).jpg"
     },
     {
         name: "Project#2",
-        tags: "applications",
+        type: "applications",
         fieldOfActivity: "",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
         imageProject: "img/projects/applications/project_app_(1).jpg"
     },
     {
         name: "Project#3",
-        tags: "web-projects",
+        type: "web-projects",
         fieldOfActivity: "",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
         imageProject: "img/projects/web-projects/project_web_(2).jpg"
     },
     {
         name: "Project#4",
-        tags: "applications",
+        type: "applications",
         fieldOfActivity: "",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
         imageProject: "img/projects/applications/project_app_(2).jpg"
     },
     {
         name: "Project#5",
-        tags: "web-projects",
+        type: "web-projects",
         fieldOfActivity: "",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
         imageProject: "img/projects/web-projects/project_web_(3).jpg"
     },
     {
         name: "Project#6",
-        tags: "applications",
+        type: "applications",
         fieldOfActivity: "",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
         imageProject: "img/projects/applications/project_app_(3).jpg"
     },
     {
         name: "Project#7",
-        tags: "web-projects",
+        type: "web-projects",
         fieldOfActivity: "",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
         imageProject: "img/projects/web-projects/project_web_(4).jpg"
     },
     {
         name: "Project#8",
-        tags: "applications",
+        type: "applications",
         fieldOfActivity: "",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
         imageProject: "img/projects/applications/project_app_(4).jpg"
     },
     {
         name: "Project#9",
-        tags: "applications",
+        type: "applications",
         fieldOfActivity: "",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
         imageProject: "img/projects/applications/project_app_(5).jpg"
     }
-]
+];
+
+const infoProject = {
+    types: ["web-projects", "applications"],
+    fieldsOfActivity: [""]
+};
