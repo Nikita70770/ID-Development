5bddac7fcada054a69594aafc5bb54f5a0171903
Nikita <filippovni1999@gmail.com>
2023-06-26 13:02:42 +0300

    Create two new files our_team.js and slider.js

---
+++ js/data/our_team.js
@@ -0,0 +1,17 @@
+let ourTeamData = [
+    {
+        title: "Lorem ipsum dolor sit amet.",
+        contentDesc: "orem ipsum dolor sit amet consectetur adipisicing elit. Beatae deleniti in ratione voluptatum ea optio inventore reprehenderit eligendi, laboriosam numquam itaque dolorem aspernatur ab illum est blanditiis dolor debitis facere.",
+        image: "img/our_team/group-people.jpg"
+    },
+    {
+        title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        contentDesc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore necessitatibus dolor ratione, minus excepturi porro cum facere labore odio atque",
+        image: "img/our_team/group-people-2.jpg"
+    },
+    {
+        title: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        contentDesc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum natus ratione distinctio vitae perferendis qui ut sit, numquam iure blanditiis, non ipsam tempora tempore ducimus.",
+        image: "img/our_team/group-people-3.jpg"
+    }
+]
