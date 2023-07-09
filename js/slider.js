64dba20751d021c4b0cb891178512ca5c038bae9
Nikita <filippovni1999@gmail.com>
2023-07-09 16:20:04 +0300

    Add a group of radio buttons in the section called our team

--- js/slider.js
+++ js/slider.js
@@ -3,20 +3,20 @@ import { ourTeam } from './data/our_team.js';
 const ourTeamImgOne = document.querySelector('.our-team-image.one');
 const ourTeamImgTwo = document.querySelector('.our-team-image.two');
 const ourTeamTitle = document.querySelector('.our-team-title');
-const ourTeamContentRightInner = document.querySelector('.our-team-content-right-inner');
+const ourTeamContentRight = document.querySelector('.our-team-content-right');
 const ourTeamContentDesc = document.querySelector('.our-team-content-desc');
+const radioBtns = document.querySelector(".block-radio-buttons").getElementsByTagName("*");
 
 let currSlide = 0;
 let counter = 0;
+let sizeArr = ourTeam.length; 
 
 function setTextContentSlide(){
     ourTeamTitle.textContent = ourTeam[currSlide].title;
     ourTeamContentDesc.textContent = ourTeam[currSlide].contentDesc;
-    enableAnimation(true);
 }
 
 function nextSlide(){
-    console.log(counter);
     if(counter % 2 == 0){ 
         ourTeamImgOne.classList.add("hide");
         ourTeamImgTwo.classList.remove("hide");
@@ -27,8 +27,11 @@ function nextSlide(){
         ourTeamImgOne.classList.remove("hide");
         ourTeamImgOne.src = ourTeam[currSlide].image;
     }
+
     setTextContentSlide();
-    currSlide = (currSlide + 1) % ourTeam.length;
+    enableAnimation();
+
+    currSlide = (currSlide + 1) % sizeArr;
     counter++;
 }
 
@@ -36,17 +39,21 @@ function enableAnimation(){
     ourTeamTitle.classList.remove("move-left");
     ourTeamTitle.classList.add("move-right");
 
-    ourTeamContentRightInner.classList.remove("move-down");
-    ourTeamContentRightInner.classList.add("move-up");
+    ourTeamContentRight.classList.remove("move-down");
+    ourTeamContentRight.classList.add("move-up");   
+
+    if(currSlide > 0) radioBtns[currSlide - 1].classList.remove("active");
+    if(currSlide == 0) radioBtns[sizeArr - 1].classList.remove("active");
+    radioBtns[currSlide].classList.add("active");
+
     setTimeout(() => {
         ourTeamTitle.classList.remove("move-right");
         ourTeamTitle.classList.add("move-left");
 
-        ourTeamContentRightInner.classList.remove("move-up");
-        ourTeamContentRightInner.classList.add("move-down");
-    }, 4000)
+        ourTeamContentRight.classList.remove("move-up");
+        ourTeamContentRight.classList.add("move-down");
+    }, 4000);
 }
 
 nextSlide();
-
 setInterval(nextSlide, 5000);
